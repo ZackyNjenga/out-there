d3658d32dfce8a419a85b3db2e44bbf6288db0c8
zacharynjenga381@gmail.com <zacharynjenga381@gmail.com>
2024-02-23 20:55:22 +0300

    little fixes

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+    return (
+    <div >
+        <header>
+            <img src="assets/logo.svg" alt="logo"/>
+        </header>
+
+    </div>
+    )
+}
+
+export default App
